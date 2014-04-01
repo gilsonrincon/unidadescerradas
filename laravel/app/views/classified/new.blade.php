9a759d545bc8b0b98471ff9f6e0943bb4f45d486
Lighting snake <andres.territoriovirtual@gmail.com>
2014-04-01 01:08:05 -0500

    Maquetación publicar clasificado
    Maquetación de publicar clasificado.

--- laravel/app/views/classified/new.blade.php
+++ laravel/app/views/classified/new.blade.php
@@ -1,74 +1,39 @@
-{{ Form::open(array('route' => 'classified.store', 'method'=>'POST', 'files'=>true), array('role'=>'form')) }}
-	<!-- Campo para el titulo -->
-	{{ Form::label('title', 'Titulo') }}
-	{{ Form::input('text', 'title') }}<br>
-	
-	<!-- Campo para la descripción corta -->
-	{{ Form::label('shortDescription', 'Descripción corta') }}<br>
-	{{ Form::textarea('shortDescription', '') }}<br>
-	
-	<!-- Campo para descripcion larga -->
-	{{ Form::label('description', 'Descripción') }}<br>
-	{{ Form::textarea('description', '') }}<br>
-	
-	<!--Es de paga -->
-	Premium:<br>
-	{{ Form::label('premium', 'No') }}
-	{{ Form::radio('premium', '0', true) }}
-	{{ Form::label('premium', 'Si') }}
-	{{ Form::radio('premium', '1') }}
-	<br>
-	<!-- Fecha de inicio -->
+@extends ('layout')
 
-	Fecha de inicio:
-	{{ Form::label('startDay', 'Día') }}
-	<select name="startDay">
-		@for ($i = 1; $i <= 31; $i++)
-			<option value="{{ $i }}">{{$i}}</option>
-		@endfor
-	</select>
+@section ('content')
+	<section>
+	<div class="title">PUBLIQUE SU CLASIFICADO</div>
+		<div class="content" style="padding: 50px 10% 30px 10%">
+			<p>años de antiguedad. Richard McClintock, un profesor de Latin de la Universidad de Hampden-2 OPCIONES, encontró una de las palabras más oscuras de la lengua del latín, "consecteur", en un pasaje de Lorem Ipsum, y al seguir leyendo distintos textos del latín, </p>
+			<div class="col-md-6 classified-form">
+				<h2>OPCION #1</h2>
+				{{Form::open()}}
+					<p>Con esta opcion podras publicar totalmente gratis en el portal del sitio y aparecera en la seccion de clasificados (Solo texto)</p>
+					<div style="text-align: right;"><span style="color: #969696">60  caracteres  disponibles</span></div>
+					<textarea class="text" name="shortDescription" cols="30" rows="10" placeholder="inserte su clasificado  (Solo texto)"></textarea>
+					{{Form::checkbox('accept_no_premium', 'accept', false, array('id' => 'accept_no_premium'))}}
+					{{Form::label('accept_no_premium', 'Acepto que quiero publicar esto en la plataforma')}}
+					{{Form::submit('PUBLICAR', array('class'=>'btn-submit-classified'))}}
+				{{Form::close()}}
+			</div>
 
-	{{ Form::label('startMonth', 'Mes') }}
-	{{ Form::select('startMonth', $months)}}
-
-	{{ Form::label('startYear', 'Año') }}
-	<select name="startYear">
-		@for ($i = 2014; $i <= 2020; $i++)
-			<option value="{{ $i }}">{{$i}}</option>
-		@endfor
-	</select>
-	<br>
-	<!-- Fecha final -->
-
-	Fecha final:
-	{{ Form::label('endDay', 'Día') }}
-	<select name="endDay">
-		@for ($i = 1; $i <= 31; $i++)
-			<option value="{{ $i }}">{{$i}}</option>
-		@endfor
-	</select>
-
-	{{ Form::label('endMonth', 'Mes') }}
-	{{ Form::select('endMonth', $months)}}
-
-	{{ Form::label('endYear', 'Año') }}
-	<select name="endYear">
-		@for ($i = 2014; $i <= 2020; $i++)
-			<option value="{{ $i }}">{{$i}}</option>
-		@endfor
-	</select>
-	<br>
-
-	<!-- Usuario -->
-	<select name="user">
-		@foreach ($users as $user)
-			<option value="{{ $user->id }}">{{ $user->username }}</option>
-		@endforeach
-	</select>
-
-	<!--Imagen -->
-	{{ Form::file('image')}}
-
-	{{ Form::button('Guardar', array('type'=>'submit'))}}
-
-{{ Form::close() }}
+			<div class="col-md-6 classified-form">
+				<h2>OPCION #2</h2>
+				{{Form::open()}}
+					<p>Con esta opcion puedes publicar y subir imagenes tiene <span style="color: #007F54">costo de $10.000</span>  y este clasificado sera enviado por correo masivo  (texto  e imagenes)</p>
+					{{Form::text('title', '', array('placeholder'=>'Mi titulo:', 'class'=>'text'))}}
+					{{Form::file('image1')}}<br>
+					{{Form::file('image2')}}<br>
+					{{Form::file('image3')}}<br>
+					<div style="text-align: right;"><span style="color: #969696">60  caracteres  disponibles</span></div>
+					{{Form::textarea('shortDescription', '', array('placeholder'=>'Descripción corta', 'class'=>'text'))}}
+					<div style="text-align: right;"><span style="color: #969696">Sin limite de caracteres</span></div>
+					{{Form::textarea('descripcion', '', array('placeholder'=>'inserte su clasificado  (Solo texto)', 'class'=>'text'))}}
+					{{Form::checkbox('accept_premium', 'accept', false, array('id' => 'accept_premium'))}}
+					{{Form::label('accept_premium', 'Acepto que quiero publicar esto en la plataforma')}}
+					{{Form::submit('PUBLICAR', array('class'=>'btn-submit-classified'))}}
+				{{Form::close()}}
+			</div>
+		</div>
+	</section>
+@stop
