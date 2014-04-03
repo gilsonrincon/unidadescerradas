8f4bf28a3a3ec86083898e5a13877b0e5c6f25e7
Lighting snake <andres.territoriovirtual@gmail.com>
2014-04-03 01:14:53 -0500

    Reservación 2
    Mejorada la validación de reservación de servicios, para mostrarla de
    una forma mas clara y precisa.

--- laravel/app/views/reservacionDeServicios.blade.php
+++ laravel/app/views/reservacionDeServicios.blade.php
@@ -5,24 +5,45 @@
 		$(document).on('ready', function(){
 			//Validamos el formulario
 			$('#submit-reservation').click(function(event) {
+				//variable que define si es valido, por defecto no lo sera
+				$is_valid = true
 				//Validamos que se seleccione un tipo de reservación
 				if($('#type-reservation').val() == 0){
-					$('.hide-content p').text('Debe seleccionar un tipo de reservación')
-					$('.hide-container').fadeIn(500);
-					event.preventDefault()
-					return false
-				}
+					$('#type-reservation').css('border', '1px solid #ff002f');
+					$is_valid = false
+				}	
 
 				//Validamos la fecha
-				if($('#date').val() == ''){
-					$('.hide-content p').text('Debe colocar una fecha completa')
-					$('.hide-container').fadeIn(500);
+				if($('#calendar').val() == ''){
+					$('#calendar').css('border', '1px solid #ff002f');
+					$is_valid = false
+				}
+
+				//Validamos que se acepten las condiciones del servicio
+				if($('#accept').is(':checked') == false){
+					$('#accept-label').css('color', '#ff002f');
+					$is_valid = false
+				}
+
+				/*En caso de no ser valido interrumpe el envio del formulario*/
+				if($is_valid == false){
+					$('.validation').css('visibility', 'visible');
 					event.preventDefault()
 					return false
 				}
+				
+			});
 
-				event.preventDefault()
-				return false
+			/*Al dar click en uno de los elementos validados regresa a su color normal*/
+			$('#type-reservation, #calendar').click(function(event) {
+				$(this).css('border', '1px solid #000')
+				$('.validation').css('visibility', 'hidden');
+			});
+
+			/*El color del texto regresa normal cuando se da click en aceptar publicar esto*/
+			$('#accept, #accept-label').click(function(event) {
+				$('#accept-label').css('color', '#000')
+				$('.validation').css('visibility', 'hidden');
 			});
 
 			//Ocultamos el hide container
@@ -34,7 +55,7 @@
 			$('#calendar').datetimepicker(
 				{
 					lang:'es',
-					format:'Y-m-d H:i',
+					format:'Y-m-d H:i:s',
 				}
 			);
 		})
@@ -49,7 +70,7 @@
 		<div id="reservations-container">
 			<div id="reservations-text" class="col-md-12">
 				<h2>
-					PARA  HACER UNA RESERVACION DE ALGUN SERVIO DE LA UNIDAD DEBES :
+					PARA  HACER UNA RESERVACION DE ALGUN SERVICIO DE LA UNIDAD DEBES :
 				</h2>
 				<p>
 					llenar el formulario de reservacion el cual sera enviado a la administracion y ya ellos te haran saber cuando puedes tener el espacio y lo cambiaran en el calendario de evento, el valor cambia dependiendo del tipo de servicio que desea reservar ...
@@ -76,18 +97,17 @@
 					<textarea id="description-reservation" class="text" name="description" cols="10" rows="10" placeholder="DESCRIPCIÓN DE LA RESERVACIÓN"></textarea>
 				</div>
 				<div class="col-md-6">
-					<input id="calendar" class="text" type="text" name="date">
+					<input id="calendar" class="text" type="text" name="date" placeholder="Fecha y hora:">
 					<input class="text" type="tel" name="tel" placeholder="TELEFONO:">
-					<label><input type="checkbox" id="accept"> Acepto que quiero publicar esto en la plataforma</label>
+					<label id="accept-label"><input type="checkbox" id="accept"> Acepto que quiero publicar esto en la plataforma</label>
 					<input id="submit-reservation" type="submit" class="btn-submit" value="Reservar">
 				</div>
 				{{ Form::close() }}
-
+			</div>
+			<div class="col-md-12">
 				{{-- Usado para mostrar errores de validación --}}
-				<div class="hide-container">
-					<div class="hide-content">
-						<p></p>
-					</div>
+				<div class="validation">
+					<p>Debe llenar adecuadamente todos los campos marcados con <span style="color: #ff002f">ROJO</span></p>
 				</div>
 			</div>
 		</div>
